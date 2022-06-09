5290cfa90356b4a1070f35e37a1c6377bc0b24f9
Terbeche Mustapha <termus96@gmail.com>
2022-06-09 12:39:31 +0100

    Add test for removing todo item from the array (local storage and DOM)

--- src/modules/__mocks__/todos.js
+++ src/modules/__mocks__/todos.js
@@ -1,10 +1,26 @@
 export const addTodo = (arr, obj)=>{
     arr.push(obj);
+    getStorageMock.setItem('arr', arr);
     return arr;
 }
 
-export const removeTodo = (arr)=>{
-  if(arr.length > 0){
-      
+
+export const removeTodo = (todos, id) => {
+  const newTodos = todos.filter((e) => e.index !== id);
+  newTodos.sort((a, b) => a.index - b.index);
+  for (let i = 1; i <= newTodos.length; i += 1) {
+    newTodos[i - 1].index = i;
   }
-}
+  return newTodos;
+};
+
+
+let storage = {};
+
+export const getStorageMock = {
+  getItem: (key) => storage[key] || null,
+  setItem: (key, value) => { storage[key] = value; },
+  removeItem: (key) => delete storage[key],
+  clear: storage = {},
+
+};
--- src/modules/add-delete-item.test.js
+++ src/modules/add-delete-item.test.js
@@ -1,29 +1,76 @@
 jest.mock('./todos')
-import { addTodo } from './todos'
+import {
+  addTodo
+} from './todos'
+import {
+  removeTodo
+} from './todos'
+import {
+  getStorageMock
+} from './todos'
 
-describe('should add an item to localstorage', ()=>{
-    let arr = [];
-    test('add a list item', ()=>{
-        let todo = {
-            description: "do laundry",
-            completed: "false",
-            index: "1"
-        }
+describe('should add an item to localstorage', () => {
+  let arr = [];
+  test('add a list item', () => {
+    let todo = {
+      description: "do laundry",
+      completed: "false",
+      index: "1"
+    }
 
-        let addTodoItem = addTodo(arr, todo);
-        expect(addTodoItem).toHaveLength(1);
+    let addTodoItem = addTodo(arr, todo);
+    expect(addTodoItem).toHaveLength(1);
 
-    });
-    test('add a list item', ()=>{
-        let todo = {
-            description: "do laundry",
-            completed: "false",
-            index: "2"
-        }
+  });
+  test('should update localstorage after adding a new item', () => {
+    expect(getStorageMock.getItem('arr')).toHaveLength(1);
+  });
+  test('add a list item', () => {
+    let todo = {
+      description: "do laundry",
+      completed: "false",
+      index: "2"
+    }
 
-        let addTodoItem = addTodo(arr, todo);
-        expect(addTodoItem).toHaveLength(2);
+    let addTodoItem = addTodo(arr, todo);
+    expect(addTodoItem).toHaveLength(2);
 
-    })
-    test('', ()=>{});
+  })
+
+  test('should update localstorage after adding a new item', () => {
+    expect(getStorageMock.getItem('arr')).toHaveLength(2);
+  });
 })
+
+
+
+describe('removetodo', () => {
+  let todo = [{
+      description: 'Wake up',
+      index: 1,
+      completed: false,
+    },
+    {
+      description: 'Eat BreakFast',
+      index: 2,
+      completed: false,
+    },
+    {
+      description: 'Finish the project',
+      index: 3,
+      completed: false,
+    },
+  ];
+
+  test('should remove todo from list', () => {
+    todo = removeTodo(todo, 2);
+    expect(getStorageMock.getItem('arr')).toHaveLength(2);
+  });
+  test('should make sure removed item is no longer in list', () => {
+    expect(getStorageMock.getItem('arr')[1].description).not.toEqual('Eat BreakFast');
+  });
+
+  test('should delete only one item from the array', () => {
+    expect(removeTodo(todo, 1)).toHaveLength(1);
+  });
+});
